5ac8373662ad2e0576a9333665d33361c1c2cd5e
Chuan <chuannguyen.vn@hcmut.edu.vn>
2022-10-22 23:56:47 +0700

    feat: pixel-perfect margins and paddings and stuff

--- Assets/_Scripts/QueryResult.cs
+++ Assets/_Scripts/QueryResult.cs
@@ -12,15 +12,15 @@ using UnityEngine.UI;
 public class QueryResult : MonoBehaviour
 {
     [SerializeField] private ScrollRect scrollRect;
-    [SerializeField] private Rectangle rectangle;
+    [SerializeField] private Rectangle innerRectangle;
+    [SerializeField] private Rectangle outerRectangle;
 
-    [FormerlySerializedAs("MaxSize")] [SerializeField]
     private Vector2 maxSize;
 
     [SerializeField] private float margin;
     public RectTransform TopPoint;
     public RectTransform BottomPoint;
-    
+
     public int Index { get; private set; }
     public string Query { get; private set; }
 
@@ -39,13 +39,11 @@ public class QueryResult : MonoBehaviour
 
         var rectTransform = GetComponent<RectTransform>();
         rectTransform.SetParent(ResourceManager.Instance.QueryResultRect.transform);
-        rectTransform.anchoredPosition = Vector2.zero;
+        maxSize = ResourceManager.Instance.QueryResultRect.GetComponent<RectTransform>().sizeDelta;
 
         List<List<Cell>> cells = new List<List<Cell>>();
         List<float> widths = Enumerable.Repeat(0f, result[0].Count).ToList();
         List<float> heights = Enumerable.Repeat(0f, result.Count).ToList();
-
-        mainPos = transform.position;
 
         for (var row = 0; row < result.Count; row++)
         {
@@ -78,16 +76,20 @@ public class QueryResult : MonoBehaviour
             heightSoFar += heights[row] + VisualManager.Instance.Padding * 2;
         }
 
-        scrollRect.content.sizeDelta = new Vector2(widthSoFar, heightSoFar);
-
-        var rectangleSize = new Vector2(Mathf.Min(widthSoFar, maxSize.x) + margin,
+        var outerRectangleSize = new Vector2(Mathf.Min(widthSoFar, maxSize.x) + margin,
             Mathf.Min(heightSoFar, maxSize.y));
 
-        rectTransform.sizeDelta = rectangleSize;
+        rectTransform.anchoredPosition = Vector2.zero;
+        rectTransform.sizeDelta = outerRectangleSize;
+        mainPos = transform.position;
 
-        scrollRect.GetComponent<RectTransform>().sizeDelta = new Vector2(-margin, 0);
-        rectangle.Width = rectangleSize.x;
-        rectangle.Height = rectangleSize.y;
+        scrollRect.content.sizeDelta = new Vector2(widthSoFar, heightSoFar);
+        scrollRect.GetComponent<RectTransform>().sizeDelta = new Vector2(-margin, -margin);
+        outerRectangle.Width = outerRectangleSize.x;
+        outerRectangle.Height = outerRectangleSize.y;
+
+        innerRectangle.Width = outerRectangleSize.x - margin;
+        innerRectangle.Height = outerRectangleSize.y - margin;
     }
 
     public void Show()
