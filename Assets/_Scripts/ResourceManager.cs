2478b9664eff12dde6190442c9371ca716ee2f4d
Chuan <chuannguyen.vn@hcmut.edu.vn>
2022-10-23 03:19:42 +0700

    feat: more colors for special words/text

--- Assets/_Scripts/ResourceManager.cs
+++ Assets/_Scripts/ResourceManager.cs
@@ -2,12 +2,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using UnityEngine;
 
 
 public class ResourceManager : PersistentSingleton<ResourceManager>
 {
+    [HideInInspector] public string[] SQLTableNames = new[]
+    {
+        "crime_scene_report",
+        "drivers_license",
+        "facebook_event_checkin",
+        "interview",
+        "get_fit_now_member",
+        "get_fit_now_check_in",
+        "solution",
+        "income",
+        "person"
+    };
+
     [HideInInspector] public string[] SQLKeywords = new[]
     {
         "ADD",
@@ -158,27 +172,161 @@ public class ResourceManager : PersistentSingleton<ResourceManager>
         }
     }
 
-    public List<(int, int)> GetKeywordIndicesAndLengths(string query)
+    public List<SpecialWord> GetKeywordIndicesAndLengths(string query, SpecialType specialType)
     {
-        List<(int, int)> indices = new List<(int, int)>();
-        query = query.ToUpper();
+        List<SpecialWord> specialWords = new List<SpecialWord>();
 
-        foreach (var keyword in SQLKeywords)
+        if (specialType == SpecialType.String)
         {
-            var index = query.IndexOf(keyword);
-            if (index == -1) continue;
+            int stringIndex = 0;
+            char stringIndicator = '~';
+            for (int i = 0; i < query.Length; i++)
+            {
+                if (query[i] == '\'')
+                {
+                    if (stringIndicator == '\'')
+                    {
+                        specialWords.Add(new SpecialWord(stringIndex, i - stringIndex + 1,
+                            SpecialType.String));
+                        stringIndicator = '~';
+                    }
+                    else
+                    {
+                        stringIndex = i;
+                        stringIndicator = '\'';
+                    }
+                }
+                else if (query[i] == '"')
+                {
+                    if (stringIndicator == '"')
+                    {
+                        specialWords.Add(new SpecialWord(stringIndex, i - stringIndex + 1,
+                            SpecialType.String));
+                        stringIndicator = '~';
+                    }
+                    else
+                    {
+                        stringIndex = i;
+                        stringIndicator = '"';
+                    }
+                }
+            }
 
-            bool start = index == 0 || !IsAlphaNumeric(query[index - 1]);
-            bool end = index + keyword.Length == query.Length || query[index + keyword.Length] == ' ';
+            if (stringIndicator is ' ' or '\n' or '"' or '\'')
+                specialWords.Add(new SpecialWord(stringIndex, query.Length - stringIndex,
+                    SpecialType.String));
+        }
+        else if (specialType == SpecialType.Number)
+        {
+            int numberIndex = 0;
+            bool lastNumNumeric = false;
+            for (int i = 0; i < query.Length; i++)
+            {
+                if (IsNumeric(query[i]))
+                {
+                    if (!lastNumNumeric)
+                    {
+                        numberIndex = i;
+                    }
 
-            if (start && end) indices.Add((index, keyword.Length));
+                    lastNumNumeric = true;
+                }
+                else
+                {
+                    if (lastNumNumeric)
+                    {
+                        if (numberIndex > 0 && !IsAlpha(query[numberIndex]) && !IsAlpha(query[i]))
+                            specialWords.Add(new SpecialWord(numberIndex, i - numberIndex,
+                                SpecialType.Number));
+                    }
+
+                    lastNumNumeric = false;
+                }
+            }
+
+            if (lastNumNumeric)
+                specialWords.Add(new SpecialWord(numberIndex, query.Length - numberIndex,
+                    SpecialType.Number));
+        }
+        else
+        {
+            if (specialType == SpecialType.Keyword) query = query.ToUpper();
+
+            string[] list = specialType switch
+            {
+                SpecialType.Keyword => SQLKeywords,
+                SpecialType.Name => SQLTableNames,
+                _ => new string[] { }
+            };
+
+            foreach (var special in list)
+            {
+                var index = query.IndexOf(special);
+                if (index == -1) continue;
+
+                bool start = index == 0 || !IsAlphaNumeric(query[index - 1]);
+                bool end = index + special.Length == query.Length ||
+                           query[index + special.Length] == ' ' || query[index + special.Length] == '\n';
+
+                if (start && end) specialWords.Add(new SpecialWord(index, special.Length, specialType));
+            }
         }
 
-        return indices;
+        return specialWords;
 
         bool IsAlphaNumeric(char c)
         {
             return Regex.IsMatch(c.ToString(), @"^[a-zA-Z0-9_]+$");
+        }
+
+        bool IsNumeric(char c)
+        {
+            return Regex.IsMatch(c.ToString(), @"^[0-9]+$");
+        }
+
+        bool IsAlpha(char c)
+        {
+            return Regex.IsMatch(c.ToString(), @"^[a-zA-Z_]+$");
+        }
+    }
+}
+
+public enum SpecialType
+{
+    Keyword,
+    Name,
+    String,
+    Number
+}
+
+public class SpecialWord
+{
+    public int Index;
+    public int Length;
+    public SpecialType SpecialType;
+
+    public SpecialWord(int index, int length, SpecialType specialType)
+    {
+        Index = index;
+        Length = length;
+        SpecialType = specialType;
+    }
+
+    public string GetColorHex()
+    {
+        switch (SpecialType)
+        {
+            case SpecialType.Keyword:
+
+                return ColorUtility.ToHtmlStringRGB(VisualManager.Instance.SQLKeywordColor);
+            case SpecialType.Name:
+                return ColorUtility.ToHtmlStringRGB(VisualManager.Instance.SQLNameColor);
+            case SpecialType.String:
+                return ColorUtility.ToHtmlStringRGB(VisualManager.Instance.SQLStringColor);
+            case SpecialType.Number:
+                return ColorUtility.ToHtmlStringRGB(VisualManager.Instance.SQLNumberColor);
+            default:
+                throw new ArgumentOutOfRangeException();
         }
     }
 }
